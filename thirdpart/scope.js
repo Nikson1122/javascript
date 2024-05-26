f9a971a3aaa7da882b53a06754c04df7cb21a3d9
Nikson1122 <104704390+Nikson1122@users.noreply.github.com>
2024-05-26 11:57:42 +0545

    scope and hoisting

---
+++ thirdpart/scope.js
@@ -0,0 +1,52 @@
+//var c = 300
+
+let a = 300
+
+if (true) {
+
+    let a = 10
+    const b = 20
+    // console.log("Inner:", a)
+     
+    
+}
+
+
+//console.log(a);
+//console.log(b);
+//console.log(c);
+
+function one(){
+    const username = "Nikson"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+   // console.log(website);
+
+   two()
+}
+//one()
+if(true){
+    const username = "Nikson"
+    if(username === "Nikson"){
+        const website = "youtube"
+        console.log(username + website);
+    }
+   // console.log(website);
+}
+//console.log(username);
+
+// +++++++++++++++++++++++++ intresting ++++++++++++++
+
+console.log(addone(5))
+function addone(num){
+    return num + 1
+}
+
+console.log(addTwo(5))
+const addTwo = function(num){
+    return num + 2
+}
+addTwo(5)
